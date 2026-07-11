6ba199795839723dd06806fd43152ffc911c38f4
Riflan Mohamed <rizlanahmd4545@gmail.com>
2026-07-11 13:28:22 +0530

    Add sectors data structure for project categorization

---
+++ src/data/sectors.ts
@@ -0,0 +1,12 @@
+export const sectors: string[] = [
+  "Schools & Education",
+  "Public Buildings",
+  "Health & Clinics",
+  "Commercial Buildings",
+  "Roads & Drainage",
+  "Water Supply & Sewerage",
+  "Renovations",
+  "Finishing Works",
+  "Retaining Walls",
+  "Private Residences",
+];
